a5de4d7a07f70c5ec79fddf0aa811376ad6f2a7a
pablochuang <pablochuang@gmail.com>
2023-02-16 08:55:29 -0500

    Add Menu details

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Heatlhy Diner')
+
+streamlit.text('Omerga 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Roclet Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
