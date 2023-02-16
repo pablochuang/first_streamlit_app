b0391c14431af762c495a86f9ba2e1dd451bde6d
pablochuang <pablochuang@gmail.com>
2023-02-16 08:55:43 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit
 
 streamlit.title('My Parents New Heatlhy Diner')
 
-streamlit.text('Omerga 3 & Blueberry Oatmeal')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Roclet Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
